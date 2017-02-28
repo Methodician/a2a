8810c2a37e7c810a798b879c7b720b8dd8ea1763
Methodician <jacobgjohnston@gmail.com>
2017-02-28 10:57:40 -0800

    Needs come in rows of 3...

--- src/app/needs/needs.component.ts
+++ src/app/needs/needs.component.ts
@@ -11,7 +11,8 @@ import { Observable } from "rxjs/Observable";
 })
 export class NeedsComponent implements OnInit {
 
-  needs: Need[] = [];
+  needs: Need[] = null;
+  setsOf3: any = null;
   //needs: Observable<Need[]> = new Observable<Need[]>(null);
   constructor(
     private needSvc: NeedService
@@ -22,7 +23,32 @@ export class NeedsComponent implements OnInit {
     this.needSvc.getNeedsByApproval(true)
       .subscribe(needs => {
         this.needs = needs;
+        this.setsOfThree();
       });
+
+  }
+
+  setsOfThree() {
+    if (this.needs.length < 1)
+      return;
+    let i = 0;
+    let row = 1;
+    let arrOfArr = [];
+    while (i < this.needs.length) {
+      let tempArr = [];
+      for (let j = 0; j < 3; j++) {
+        if (this.needs[i]) {
+          tempArr.push(this.needs[i])
+        }
+        i++;
+      }
+      arrOfArr.push(tempArr);
+      tempArr = [];
+      /*if(i+1 % 3 === 0 || i === 0){
+        arrOfArr.push
+      }*/
+    }
+    this.setsOf3 = arrOfArr;
   }
 
 }
