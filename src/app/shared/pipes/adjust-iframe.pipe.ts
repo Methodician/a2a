a506628edc09dc0396f80574620c731290db89f6
Kyle Lange <baronsintrees@gmail.com>
2017-04-12 15:19:34 -0700

    fix iframe video styles for home page and needs page.

--- src/app/shared/pipes/adjust-iframe.pipe.ts
+++ src/app/shared/pipes/adjust-iframe.pipe.ts
@@ -7,7 +7,7 @@ export class AdjustIframePipe implements PipeTransform {
 
   transform(iframeHTML) {
 
-    return iframeHTML.replace('width="560" height="315"', 'width="100%"');
+    return iframeHTML.replace('width="560" height="315"', 'width="100%" height="100%"');
   }
 
 }
