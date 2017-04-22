f25baf451744a28f1e2e93f35f57eb26e860fe58
Methodician <jacobgjohnston@gmail.com>
2017-04-22 16:31:21 -0700

    More refined financial section

--- src/app/review-financials/review-financials.component.ts
+++ src/app/review-financials/review-financials.component.ts
@@ -14,6 +14,11 @@ export class ReviewFinancialsComponent implements OnInit {
   private selectedUser: any;
 
   private total: number;
+  private fees: number;
+  private subtotal: number;
+  private payouts: number;
+
+  private contributionIds: string[];
 
   constructor(
     private userSvc: UserService,
@@ -28,32 +33,76 @@ export class ReviewFinancialsComponent implements OnInit {
   }
 
   selectUser(user) {
-    this.calculateContributionTotal(user.$key);
-    this.userSvc.getClosedInfo(user.$key).take(1).subscribe(details => {
-      this.userSvc.isOrgApproved(user.$key).take(1).subscribe(approved => {
+    let id = user.$key
+    this.getContributions(id);
+    //this.calculateContributionTotal();
+    this.userSvc.getClosedInfo(id).take(1).subscribe(details => {
+      this.userSvc.isOrgApproved(id).take(1).subscribe(approved => {
         this.selectedUser = Object.assign(user, details, { isApproved: approved });
       })
     });
 
   }
 
-  calculateContributionTotal(id) {
+  getContributions(id) {
     this.finSvc.getContributionsPerOrg(id).subscribe(ids => {
-      this.total = 0;
-      for (let id of ids) {
-        this.finSvc.getContributionTotal(id.$key).subscribe(contribution => {
-          if (contribution.$value)
-            this.total += parseFloat(contribution.$value);
-        });
-      }
+      this.contributionIds = ids.map(id => id.$key);
+      //this.calculateContributionTotal();
+      this.calculateTotals();
     });
+  }
+
+  calculateTotals() {
+    this.total = 0;
+    this.subtotal = 0;
+    this.fees = 0;
+    this.payouts = 0;
+    for (let id of this.contributionIds) {
+      this.finSvc.getContributionTotal(id).subscribe(contribution => {
+        if (contribution.$value)
+          this.total += parseFloat(contribution.$value);
+      });
+      this.finSvc.getContributionFee(id).subscribe(fee => {
+        if (fee.$value)
+          this.fees += parseFloat(fee.$value);
+      });
+      this.finSvc.getContributionSubtotal(id).subscribe(subtotal => {
+        if (subtotal.$value)
+          this.subtotal += parseFloat(subtotal.$value);
+      });
+    }
+    if (this.selectedUser) {
+      this.finSvc.getPayoutsPerOrg(this.selectedUser.$key).subscribe(payouts => {
+        console.log(payouts);
+        for (let pay of payouts) {
+          this.payouts += parseFloat(pay.$value);
+        }
+      });
+    }
+  }
+
+  a2aFee() {
+    return this.total * 0.05;
+  }
+
+  payoutTotal() {
+    return this.subtotal - this.a2aFee();
+  }
+
+  pendingPayouts() {
+    return this.payoutTotal() - this.payouts;
+  }
+
+  recordPayout(amount: number) {
+    this.finSvc.recordPayout(this.selectedUser.$key, amount);
+    this.calculateTotals();
   }
 
 
   //TEMP POSSIBLY USEFUL CODE FOR TRANSFERRING CONTRIBUTIONS BETWEEN ORG
-  moveContributionOrgNode(oldKey) {
+  /*moveContributionOrgNode(oldKey) {
     this.finSvc.moveContributionOrgNode(oldKey, this.selectedUser.$key);
-  }
+  }*/
 
 
 }
