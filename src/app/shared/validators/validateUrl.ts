60fed6762b8caebd6d48da9c70f1c1a7992ca5fd
Methodician <jacobgjohnston@gmail.com>
2017-04-24 17:47:02 -0700

    sdfsdaf

--- src/app/shared/validators/validateUrl.ts
+++ src/app/shared/validators/validateUrl.ts
@@ -3,9 +3,10 @@ import { FormControl } from '@angular/forms';
 export function validateUrl(ctrl: FormControl) {
 
     const urlValue = ctrl.value;
+    const touched = ctrl.touched;
 
     //  I should probably learn more about regular expressions
-    const valid = /^(ftp|http|https):\/\/[^ "]+$/.test(urlValue);
+    const valid = /^(ftp|http|https):\/\/[^ "]+$/.test(urlValue) || !touched;
 
     return valid ? null : {
         validUrl: {
